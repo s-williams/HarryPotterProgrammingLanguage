4c2135ae749dd4ee0296d49d37728154c6b9d0c7
SWilliams <SWilliams>
2017-03-07 13:47:09 +0000

    listLength

--- HPPL.ml
+++ HPPL.ml
@@ -35,8 +35,6 @@ let rec insert_at x n = function
     | [] -> [x]
     | h :: t as l -> if n = 0 then x :: l else h :: insert_at x (n-1) t;;
 
-(* Returns the size of a list *)
-
 (* Checks if two lists are the same *)
 
 (* Is a list longer than x *)
